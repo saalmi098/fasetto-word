d84b0b2a7dc8492b1a5ef0bd57169931d153d575
saalmi098 <andisalminger@hotmail.com>
2018-03-05 10:57:43 +0100

    08 - Advanced View Models Real World

---
+++ Source/Fasetto.Word/ViewModel/Base/RelayParameterizedCommand.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace Fasetto.Word
+{
+    /// <summary>
+    /// A basic command that runs an action
+    /// </summary>
+    public class RelayParameterizedCommand : ICommand
+    {
+        /// <summary>
+        /// The action to run
+        /// </summary>
+        private Action<object> mAction;
+
+        /// <summary>
+        /// The event that fires when the <see cref="CanExecute(object)"/> value has changed
+        /// </summary>
+        public event EventHandler CanExecuteChanged = (sender, e) => { };
+
+        public RelayParameterizedCommand(Action<object> action)
+        {
+            mAction = action;
+        }
+
+        public bool CanExecute(object parameter) => true;
+
+        public void Execute(object parameter)
+        {
+            mAction?.Invoke(parameter);
+        }
+    }
+}
---
+++ Source/Fasetto.Word/ViewModel/LoginViewModel.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Security;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace Fasetto.Word
+{
+    /// <summary>
+    /// View model for a login screen
+    /// </summary>
+    public class LoginViewModel : BaseViewModel
+    {
+        #region Public Properties
+
+        /// <summary>
+        /// The email of the user
+        /// </summary>
+        public string Email { get; set; }
+
+        /// <summary>
+        /// A flag indicating if the <see cref="LoginCommand"/> is running
+        /// </summary>
+        public bool LoginIsRunning { get; set; }
+
+        #endregion
+
+        #region Commands
+
+        /// <summary>
+        /// The command to login
+        /// </summary>
+        public ICommand LoginCommand { get; set; }
+
+        #endregion
+
+        #region Constructor
+
+        public LoginViewModel()
+        {
+            // Create commands
+            LoginCommand = new RelayParameterizedCommand(async (parameter) => await Login(parameter));
+        }
+
+        #endregion
+
+        /// <summary>
+        /// Attempts to log the user in
+        /// </summary>
+        /// <param name="parameter">The <see cref="SecureString"/> passed in from the view for the users password</param>
+        /// <returns></returns>
+        public async Task Login(object parameter)
+        {
+            await RunCommand(() => LoginIsRunning, async () =>
+            {
+                await Task.Delay(20000);
+
+                var email = Email;
+                var x = (parameter as IHavePassword).SecurePassword.Unsecure();
+            });
+        }
+    }
+}
