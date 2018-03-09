80480a63dad6549b8c901563108cf52c0e7b19b6
saalmi098 <andisalminger@hotmail.com>
2018-03-09 11:22:10 +0100

    11.2 - Dependency Injection & Multiple Projects

---
+++ Source/Fasetto.Word.Core/IOC/IoC.cs
@@ -0,0 +1,53 @@
+﻿using Ninject;
+using System;
+
+namespace Fasetto.Word.Core
+{
+    /// <summary>
+    /// The IoC container for our application
+    /// </summary>
+    public static class IoC
+    {
+        #region Public Properties
+
+        /// <summary>
+        /// The kernel for our IoC container
+        /// </summary>
+        public static IKernel Kernel { get; private set; } = new StandardKernel();
+
+        #endregion
+
+        #region Construction
+
+        /// <summary>
+        /// Sets up the IoC container, binds all information required and is ready for use
+        /// Must be called as soon as your application starts up to ensure all services can be found
+        /// </summary>
+        public static void Setup()
+        {
+            // Bind all required view models
+            BindViewModels();
+        }
+
+        /// <summary>
+        /// Binds all singleton view models
+        /// </summary>
+        private static void BindViewModels()
+        {
+            // Bind to a single instance of application view model
+            Kernel.Bind<ApplicationViewModel>().ToConstant(new ApplicationViewModel());
+        }
+
+        #endregion
+
+        /// <summary>
+        /// Gets a service from the IoC, of the specified type
+        /// </summary>
+        /// <typeparam name="T">Type to get</typeparam>
+        /// <returns></returns>
+        public static T Get<T>()
+        {
+            return Kernel.Get<T>();
+        }
+    }
+}
---
+++ Source/Fasetto.Word/ViewModels/ViewModelLocator.cs
@@ -0,0 +1,30 @@
+﻿using Fasetto.Word.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fasetto.Word
+{
+    /// <summary>
+    /// Locates view models from the IoC for use in binding in XAML files
+    /// </summary>
+    public class ViewModelLocator
+    {
+        #region Public Properties
+
+        /// <summary>
+        /// A singleton instance of the locator
+        /// </summary>
+        public static ViewModelLocator Instance { get; private set; } = new ViewModelLocator();
+
+        /// <summary>
+        /// The application view model
+        /// </summary>
+        public static ApplicationViewModel ApplicationViewModel => IoC.Get<ApplicationViewModel>();
+
+        #endregion
+
+    }
+}
